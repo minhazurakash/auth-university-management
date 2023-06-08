b98ad81c53a70361a8ba752691ee83cc32ff8d24
Akash7766 <mraakash01932@gmail.com>
2023-06-09 00:14:30 +0600

    log organise date wise and delete old log

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,6 +1,7 @@
 import { createLogger, format, transports } from 'winston'
 const { combine, timestamp, label, printf } = format
 import path from 'path'
+import DailyRotateFile from 'winston-daily-rotate-file'
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
   const date = new Date(timestamp)
@@ -15,11 +16,20 @@ export const logger = createLogger({
   format: combine(label({ label: 'Mewww!' }), timestamp(), myFormat),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new transports.File({
-      filename: path.join(process.cwd(), 'Log', 'winston', 'success.log'),
-      level: 'info',
-    }),
     new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'Log',
+        'winston',
+        'success',
+        'UMS-%DATE%-Success.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
   ],
 })
 export const errorLogger = createLogger({
@@ -27,10 +37,19 @@ export const errorLogger = createLogger({
   format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new transports.File({
-      filename: path.join(process.cwd(), 'Log', 'winston', 'error.log'),
-      level: 'error',
-    }),
     new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'Log',
+        'winston',
+        'error',
+        'UMS-%DATE%-error.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
   ],
 })
