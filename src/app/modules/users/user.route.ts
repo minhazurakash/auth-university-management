82a4ce4abde35f5f4efebf324fea590f545de99d
Akash7766 <mraakash01932@gmail.com>
2023-06-15 01:29:57 +0600

    zod validation added routes level

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 import { UserController } from './user.controller'
+import validateRequest from '../../midleware/validateRequest'
+import { UserValidation } from './user.validation'
 
 const router = express.Router()
 
-router.post('/create-user', UserController.createUserController)
+router.post(
+  '/create-user',
+  validateRequest(UserValidation.createUserZodSchema),
+  UserController.createUserController
+)
 
 export const UserRoutes = router
