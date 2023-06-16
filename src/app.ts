f98a38b13c161d3f35c03e16f8ccf22f86cc51e9
Akash7766 <mraakash01932@gmail.com>
2023-06-17 00:10:47 +0600

    not found route implement

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 const app: Application = express()
 import cors from 'cors'
 import globalErrorHandler from './app/midleware/globalErrorHandler'
 import Routes from './app/routes'
+import httpStatus from 'http-status'
 
 // use middleware
 app.use(cors())
@@ -17,5 +18,13 @@ app.get('/', async (req: Request, res: Response) => {
   res.send('App running')
 })
 app.use(globalErrorHandler)
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Route not found',
+    errorMessage: [{ path: req.originalUrl, message: 'Route not found' }],
+  })
+  next()
+})
 
 export default app
