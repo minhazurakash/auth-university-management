d60ec1cdefed59171425309a81aee87b5da5b008
Akash7766 <mraakash01932@gmail.com>
2023-06-16 21:23:29 +0600

    semester name and code not matching solved

--- src/app/modules/academicSemester/academicSemester.constant.ts
+++ src/app/modules/academicSemester/academicSemester.constant.ts
@@ -16,3 +16,9 @@ export const AcademicSemesterConstant = {
     'December',
   ],
 }
+
+export const academicSemesterTitleCodeMapper: { [key: string]: string } = {
+  Autumn: '01',
+  Summer: '02',
+  Fall: '03',
+}
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,18 @@
+import ApiError from '../../../error/ApiError'
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant'
 import { IAcademicSemester } from './academicSemester.interface'
 import { AcademicSemester } from './academicSemester.model'
+import httpStatus from 'http-status'
 
 const createAcademicSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'semester title and code not matching'
+    )
+  }
   const result = await AcademicSemester.create(payload)
   return result
 }
